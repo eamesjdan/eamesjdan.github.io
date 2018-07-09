a9763eb43cba7d1503bb95b9d68d2a25d6807a3a
Dan <eamesjdaniel@gmail.com>
2018-07-09 22:21:53 +0200

    commit 3 copenhagen

--- sketch.js
+++ sketch.js
@@ -1,11 +1,21 @@
+
 function setup() {
   createCanvas(600, 400);
 }
 
 function draw() {
-  background('green');
-  ellipse(mouseX,mouseY,40,40);
-  textSize(32);
+  background('black');
+  //ellipse(mouseX,mouseY,10,10);
+  noStroke();
+  fill("blue");
+  triangle(mouseX-20,mouseY-20, 60, 0, 0, 60);
+  triangle(0,0, 60, 0, 0, 60);
+  triangle(mouseX+20,mouseY+20, 540, 400, 600, 340);
+  triangle(600,400, 540, 400, 600, 340);
+  textSize(20);
   fill("red");
-  text("Copenhagen",50,100);
+  //translate(-80,80);
+  //rotate(QUARTER_PI);
+  text("CIID 2018",mouseX,mouseY);
+
 }
